f84d3f96b40aa23837c54a70a22612553224734a
Romm17 <romanysik@gmail.com>
2016-05-20 23:22:31 +0300

    done

--- badServer.js
+++ badServer.js
@@ -2,91 +2,133 @@
 var http = require('http'),
     fs = require('fs');
 
-// Cache
-var cache = {};
+// File System
+var getPerson = function(callback) {
+  fs.readFile('./person.json', function(err, data) {
+    if (!err) {
+      callback(stringifyPerson(JSON.parse(data)));
+    } else {
+      callback(null);
+    }
+  });
+}
 
-// HTTP Server
-http.createServer(function (req, res) {
+var setPerson = function(data, req, callback) {
+  fs.writeFile('./person.json', data, function(err) {
+    if (!err) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+}
 
-  // Parse cookies
-  var cookie = req.headers.cookie,
-      cookies = {};
+// Parsing
+var parsePerson = function(data) {
+  var obj = JSON.parse(data);
+  if (obj.name) obj.name = obj.name.trim();
+  data = JSON.stringify(obj);
+  return data;
+}
+
+var stringifyPerson = function(obj) {
+  if (!obj.age) {
+    obj.birth = new Date(obj.birth);
+    var difference = new Date() - obj.birth;
+    obj.age = Math.floor(difference / 31536000000);
+    delete obj.birth;
+  }
+  return JSON.stringify(obj);
+}
+
+var parseCookie = function(cookie) {
+  var cookies = {};
   if (cookie) cookie.split(';').forEach(function(item) {
     var parts = item.split('=');
     cookies[(parts[0]).trim()] = (parts[1] || '').trim();
   });
+  return cookies;
+}
 
-  // Logging
+var strReplace = function(str, args) {
+  args.forEach(function(arg){
+    str = str.replace("%%", arg)
+  });
+  return str;
+}
+
+// HTTP
+var concatBuffer = function(req, callback) {
+  var body = [];
+  req.on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    var data = Buffer.concat(body).toString();
+    callback(data);
+  });
+}
+
+var sendResponse = function(res, status, result) {
+  res.writeHead(status);
+  res.end(result);
+}
+
+var routing = {
+  'GET/' : function(req, res, cookies) {
+    res.writeHead(200, {
+      'Set-Cookie': 'mycookie=test',
+      'Content-Type': 'text/html'
+    });
+    var ip = req.connection.remoteAddress;
+    res.write(strReplace('<h1>Welcome</h1>Your IP: %%', [ip]));
+    res.end(strReplace('<pre>%%</pre>', [JSON.stringify(cookies)]));
+    return true;
+  },
+  'GET/person' : function(req, res) {
+    getPerson(function(data){
+      sendResponse(res, data ? 200 : 500, data ? data : 'Read error');
+    });
+    return true;
+  },
+  'POST/person' : function(req, res) {
+    concatBuffer(req, function(data) {
+      data = parsePerson(data);
+      cache[req.url] = data;
+      setPerson(data, req, function(success) {
+        sendResponse(res, success ? 200 : 500, success ? 'File saved' : 'Write error');
+      });
+    });
+    return true;
+  },
+  'cached' : function(res, data) {
+    sendResponse(res, 200, data);
+    return true;
+  },
+  'default' : function(res) {
+    sendResponse(res, 404, 'Path not found');
+    return true;
+  }
+}
+
+var route = function(req, res, cookies) {
+  var metaUrl = req.method + req.url;
+  return cache[metaUrl] && routing['cached'](res, cache[metaUrl])
+   || routing[metaUrl] && routing[metaUrl](req, res, cookies)
+   || routing['default'](res);
+}
+
+var logRequest = function(req) {
   var date = new Date().toISOString();
   console.log([date, req.method, req.url].join('  '));
+}
 
-  // Serve from cache
-  if (cache[req.url] && req.method === 'GET') {
-    res.writeHead(200);
-    res.end(cache[req.url]);
-  } else {
+// Logging
+var cache = {};
 
-    // Routing
-    if (req.url === '/') {
-      if (req.method === 'GET') {
-        res.writeHead(200, {
-          'Set-Cookie': 'mycookie=test',
-          'Content-Type': 'text/html'
-        });
-        var ip = req.connection.remoteAddress;
-        res.write('<h1>Welcome</h1>Your IP: ' + ip);
-        res.end('<pre>' + JSON.stringify(cookies) + '</pre>');
-      }
-    } else if (req.url === '/person') {
-      if (req.method === 'GET') {
+http.createServer(function (req, res) {
 
-        // Some business logic
-        fs.readFile('./person.json', function(err, data) {
-          if (!err) {
-            var obj = JSON.parse(data);
-            obj.birth = new Date(obj.birth);
-            var difference = new Date() - obj.birth;
-            obj.age = Math.floor(difference / 31536000000);
-            delete obj.birth;
-            var data = JSON.stringify(obj);
-            cache[req.url] = data;
+  var cookies = parseCookie(req.headers.cookie);
+  logRequest(req);
+  route(req, res, cookies);
 
-            // HTTP reply
-            res.writeHead(200);
-            res.end(data);
-          } else {
-            res.writeHead(500);
-            res.end('Read error');
-          }
-        });
-        
-      } else if (req.method === 'POST') {
-
-        // Receiving POST data
-        var body = [];
-        req.on('data', function(chunk) {
-          body.push(chunk);
-        }).on('end', function() {
-          var data = Buffer.concat(body).toString();
-          var obj = JSON.parse(data);
-          if (obj.name) obj.name = obj.name.trim();
-          data = JSON.stringify(obj);
-          cache[req.url] = data;
-          fs.writeFile('./person.json', data, function(err) {
-            if (!err) {
-              res.writeHead(200);
-              res.end('File saved');
-            } else {
-              res.writeHead(500);
-              res.end('Write error');
-            }
-          });
-        });
-      }
-    } else {
-      res.writeHead(404);
-      res.end('Path not found');
-    }
-  }
-
-}).listen(80);
+}).listen(8800);
