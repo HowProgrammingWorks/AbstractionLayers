94f1704f86435d31d8a02e2388f196159a7e7db9
VitaliyVinterle <vinterle@yandex.ru>
2016-06-08 22:56:02 +0300

    Add files via upload

---
+++ route.js
@@ -0,0 +1,32 @@
+ var person = require('./person'),
+	   badServer = require('./badServer');
+ 
+
+exports.startRouting = function(req,res,cookies) {
+ 
+ // Serve from cache
+  if (badServer.cache[req.url] && req.method === 'GET') {
+    res.writeHead(200);
+    res.end(badServer.cache[req.url]);
+  } else {
+
+    // Routing
+    if (req.url === '/') {
+      if (req.method === 'GET') {
+        res.writeHead(200, {
+          'Set-Cookie': 'mycookie=test',
+          'Content-Type': 'text/html'
+        });
+        var ip = req.connection.remoteAddress;
+        res.write('<h1>Hello</h1>IP is: ' + ip);
+        res.end('<pre>' + JSON.stringify(cookies) + '</pre>');
+      }
+    } else if (req.url === '/person') {
+     person.execute(req, res);
+    } else {
+      res.writeHead(404);
+      res.end('Path not found');
+    }
+  }
+  
+}
